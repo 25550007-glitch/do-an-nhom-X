fa7439db35307882f9843eded80376a4577e7b1a
vy.nn <vy.nn@mekongpetro.com>
2025-10-20 22:26:40 +0700

    save

--- api/get_luong.php
+++ api/get_luong.php
@@ -2,25 +2,28 @@
 include 'db.php';
 
 $thang = isset($_GET['thang']) ? intval($_GET['thang']) : date('n');
+$nam = isset($_GET['nam']) ? intval($_GET['nam']) : date('Y');
 
-// Chỉ lấy lương của tháng được chọn
-$sql = "SELECT 
+// Chỉ lấy dữ liệu đã được tính trong bảng Lương
+$sql = "
+    SELECT 
         l.MaLuong,
         l.MaNV,
         nv.HoTen,
         pb.TenPhongBan,
         l.LuongCB,
-        l.TheoGio,
+        l.TongGioLam,
         l.TangCa,
         l.Thuong,
         l.PhuCap,
         l.KhauTru,
-        (l.LuongCB + l.TheoGio + l.TangCa + l.Thuong + l.PhuCap - l.KhauTru) AS TongLuong
+        l.TongLuong
     FROM Luong l
     LEFT JOIN NhanVien nv ON l.MaNV = nv.MaNV
     LEFT JOIN PhongBan pb ON nv.MaPB = pb.MaPB
-    WHERE l.Thang = $thang
-    ORDER BY l.MaLuong ASC";
+    WHERE l.Thang = $thang AND l.Nam = $nam
+    ORDER BY l.MaLuong ASC
+";
 
 $result = $conn->query($sql);
 $data = [];
